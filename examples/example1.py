4c7dfefdcd78e68ca2a212e20f999cc010cf02d9
Antonizoon <antonizoon@bibanon.org>
2015-11-12 13:40:08 -0500

    fix examples

--- examples/example1.py
+++ examples/example1.py
@@ -6,10 +6,7 @@ def main():
 	v = py8chan.Board('v')
 	threads = v.get_threads()
 	print("Got %i threads" % len(threads))
-
-	# pages on 8chan start at 0, not 1
-	first_page = 0
-	thread = threads[first_page]
+	thread = threads[0]
 
 	# thread information
 	print(thread)
